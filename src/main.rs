bde284d6cc7e55197f2e4b901fd8aaab9c445a41
Claire Stott <clairexstott@gmail.com>
2023-03-26 13:21:16 +0100

    image folder name contains screenshot resolution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use macroquad::prelude::*;
 use std::time::Instant;
 
-use mandlebrot::RenderMode;
+use mandlebrot::{RenderMode, ScreenDimensions};
 
 #[allow(unused_imports)]
 use mandlebrot::Visualiser;
@@ -20,13 +20,14 @@ async fn main() {
         500.0, 
         RenderMode::Coloured3D,
         (600, 600),
-        (3840, 2160) // 1080p = (1920, 1080) 4k = (3840, 2160)
+        ScreenDimensions::tuple_4k()
     );
     // let mut visualiser = Buhddabrot::new(0.005, 2_500., 50_000_000, true);
 
     // visualiser.load(0.002, -1.15, 0., 2_500.);
     // visualiser.load((0.308400934550109715351-0.308400934548031413847)/600., 0.5*(0.308400934550109715351+0.308400934548031413847), 0.5*(0.0252645634954311029242+0.0252645634938723767962), 2500.0);
     // visualiser.load(0.005, 0.3080738405277603, 0.022720381308498527, 600.0);
+    // visualiser.load( 0.000000000007506999894187192, -1.7478569335479708, 2.0020200029685542e-5, 500.);
 
     let now = Instant::now();
     visualiser.generate_image();
