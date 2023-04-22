23ff46074e0bbe2aadaf179316472f85655cf6b9
Connor Stott <connorbstott@gmail.com>
2023-04-22 21:25:42 +0100

    arbitrary precision complex numbers

--- src/complex.rs
+++ src/complex.rs
@@ -1,4 +1,6 @@
 use std::ops::{Add, Mul, Div, Sub};
+use astro_float::BigFloat;
+use astro_float::RoundingMode;
 
 fn factorial(n: u32) -> u32 {
     let mut result = 1;
@@ -10,6 +12,8 @@ fn factorial(n: u32) -> u32 {
 
 fn choose(n: u32, r: u32) -> u32 {
     assert!(n >= r);
+    if r == 0 || r == n {return 1}
+    if r == 1 || r == n-1 {return n}
     factorial(n) / (factorial(r)*factorial(n-r))
 }
 
@@ -123,6 +127,125 @@ impl Div<f64> for Complex {
     }
 }
 
+const P: usize = 128;
+const RM: RoundingMode = RoundingMode::ToEven;
+#[derive(Debug, Clone, PartialEq)]
+pub struct BigComplex {
+    real: BigFloat,
+    im: BigFloat
+}
+impl BigComplex {
+    pub fn new(real: BigFloat, im: BigFloat) -> BigComplex {
+        BigComplex { real, im }
+    }
+    
+    /// creates a new BigComplex number from a 
+    /// given normal complex number
+    pub fn from_complex(c: Complex) -> BigComplex {
+        BigComplex {
+            real: BigFloat::from_f64(c.real, P),
+            im: BigFloat::from_f64(c.im, P)
+        }
+    }
+
+    /// creates a new BigComplex number from
+    /// given f64 numbers
+    pub fn from_f64s(real: f64, im: f64) -> BigComplex {
+        BigComplex {
+            real: BigFloat::from_f64(real, P),
+            im: BigFloat::from_f64(im, P)
+        }
+    }
+
+    pub fn square(&self) -> Self {
+        BigComplex::new(
+            self.real.mul(&self.real, P, RM).sub(
+                &self.im.mul(&self.im, P, RM), P, RM
+            ),
+            BigFloat::from_u8(2, P).mul(&self.real, P, RM).mul(&self.im, P, RM)
+        )
+    }
+
+    pub fn abs_squared(&self) -> BigFloat {
+        self.real.powi(2, P, RM).add(&self.im.powi(2, P, RM), P, RM)
+    }
+
+    pub fn conjugate(&self) -> BigComplex {
+        BigComplex::new(
+            self.real.mul(&BigFloat::from_i8(1, P), P, RM),
+            self.im.mul(&BigFloat::from_i8(-1, P), P, RM)
+        )
+    }
+}
+impl Add for BigComplex {
+    type Output = BigComplex;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        BigComplex {
+            real: self.real.add(&rhs.real, P, RM),
+            im: self.im.add(&rhs.im, P, RM)
+        }
+    }
+}
+impl Sub for BigComplex {
+    type Output = BigComplex;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        BigComplex {
+            real: self.real.sub(&rhs.real, P, RM),
+            im: self.im.sub(&rhs.im, P, RM)
+        }
+    }
+}
+impl Mul for BigComplex {
+    type Output = BigComplex;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        BigComplex {
+            real: self.real.mul(&rhs.real, P, RM).sub(
+                &self.im.mul(&rhs.im, P, RM), P, RM
+            ),
+            im: self.real.mul(&rhs.im, P, RM).add(
+                &self.im.mul(&rhs.real, P, RM), P, RM
+            )
+        }
+    }
+}
+impl Mul<f64> for BigComplex {
+    type Output = BigComplex;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        let rhs = BigFloat::from_f64(rhs, P);
+        BigComplex {
+            real: self.real.mul(&rhs, P, RM),
+            im: self.im.mul(&rhs, P, RM)
+        }
+    }
+}
+impl Div for BigComplex {
+    type Output = BigComplex;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let n = self * rhs.conjugate();
+        let d = rhs.abs_squared();
+        BigComplex {
+            real: n.real.div(&d, P, RM),
+            im: n.im.div(&d, P, RM)
+        }
+    }
+}
+impl Div<f64> for BigComplex {
+    type Output = BigComplex;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        let rhs = BigFloat::from_f64(rhs, P);
+        BigComplex {
+            real: self.real.div(&rhs, P, RM),
+            im: self.im.div(&rhs, P, RM)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -135,9 +258,23 @@ mod tests {
     }
 
     #[test]
-    fn sqauare() {
+    fn add_bigcomplex() {
+        let a = BigComplex::from_f64s(1f64, 2f64);
+        let b = BigComplex::from_f64s(3f64, 4f64);
+        assert_eq!(a + b, BigComplex::from_f64s(4f64, 6f64));
+    }
+
+    #[test]
+    fn square() {
         let a = Complex::new(1f64, 2f64);
         assert_eq!(a.square(), Complex::new(-3f64, 4f64));
+    }
+
+    #[test]
+    fn square_bigcomplex() {
+        let a = BigComplex::from_f64s(1., 2.);
+        let a2 = BigComplex::from_f64s(-3., 4.);
+        assert_eq!(a.square(), a2);
     }
 
     #[test]
@@ -159,10 +296,29 @@ mod tests {
     }
 
     #[test]
+    fn bigcomplex_times() {
+        let a = BigComplex::from_f64s(3., 5.);
+        let b = BigComplex::from_f64s(2., 7.);
+
+        let answer = BigComplex::from_f64s(-29., 31.);
+
+        assert_eq!(a * b, answer);
+    }
+
+    #[test]
     fn complex_times_float() {
         let a = Complex::new(3., 6.);
         
         let answer = Complex::new(6., 12.);
+
+        assert_eq!(a * 2., answer);
+    }
+
+    #[test]
+    fn bigcomplex_times_float() {
+        let a = BigComplex::from_f64s(3., 6.);
+        
+        let answer = BigComplex::from_f64s(6., 12.);
 
         assert_eq!(a * 2., answer);
     }
@@ -182,6 +338,15 @@ mod tests {
         let a = Complex::new(3., 18.);
 
         let answer = Complex::new(1., 6.);
+
+        assert_eq!(a / 3., answer);
+    }
+    
+    #[test]
+    fn bigcomplex_divide_float() {
+        let a = BigComplex::from_f64s(3., 18.);
+
+        let answer = BigComplex::from_f64s(1., 6.);
 
         assert_eq!(a / 3., answer);
     }
